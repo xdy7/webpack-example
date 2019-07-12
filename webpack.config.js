dc669de0cab586e246d185d04ae43c27211db750
xiaodanyang <164540455@qq.com>
2019-07-12 19:50:48 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+    entry: {
+        index: './src/index.js'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.join(__dirname, 'dist')
+    },
+
+    // module: {
+    //     rules: [{
+    //         test: /\.html$/,
+    //         loader: 'underscore-template-loader'
+    //     }]
+    //     // 局部加载 html，应该不会用到
+    //     // 自定义模板文件
+    // },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/index.html',
+            title: '首页',
+            // favicon: 'favicon.ico',
+            
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'static/about.html',
+            template: 'src/page/about.html'
+        }),
+    ]
+}
